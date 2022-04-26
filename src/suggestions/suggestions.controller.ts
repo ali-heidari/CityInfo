3e67cbea989c675fdc80a60267f768f7e162884c
Ali Heidari <ali-heidari@outlook.com>
2022-04-26 22:07:40 +0430

    Throw exception if query is null or empty

--- src/suggestions/suggestions.controller.ts
+++ src/suggestions/suggestions.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { RequiredParameterException } from '../exceptions';
 import { City } from './schemas/city.schema';
 import { SuggestionsService } from './suggestions.service';
 
@@ -11,6 +12,11 @@ export class SuggestionsController {
         @Query('coordinate') coordinate: string,
         @Query('radius') radius: string,
         @Query('sort') sort: string): Promise<City[]> {
+
+        if (!query) {
+            throw new RequiredParameterException("Query can be complete or partial!");
+        }
+
         return await this.suggestionsService.findAll();
     }
 }
