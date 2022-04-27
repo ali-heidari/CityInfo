07b2e3742449f40d28552fcc3858e7ceae41e02e
Ali Heidari <ali-heidari@outlook.com>
2022-04-27 23:46:04 +0430

    Config rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SuggestionsModule } from './suggestions/suggestions.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/test'),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 5,
+    }),
     SuggestionsModule
   ]
 })
--- src/suggestions/suggestions.module.ts
+++ src/suggestions/suggestions.module.ts
@@ -3,10 +3,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { SuggestionsController } from './suggestions.controller';
 import { SuggestionsService } from './suggestions.service';
 import { City, CitySchema } from './schemas/city.schema';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: City.name, schema: CitySchema }])],
     controllers: [SuggestionsController],
-    providers: [SuggestionsService]
+    providers: [SuggestionsService,
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard,
+        }],
 })
 export class SuggestionsModule { }
